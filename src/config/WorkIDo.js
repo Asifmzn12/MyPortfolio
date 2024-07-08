982eec08acca183a1e2df5a563299b92e26e0812
Asif Ali <aliiasif3958@gmail.com>
2024-07-08 10:47:42 +0530

    changes into workido

--- src/config/WorkIDo.js
+++ src/config/WorkIDo.js
@@ -1,44 +1,35 @@
-import { FaPaintbrush, FaDesktop,  FaMobileScreenButton, } from "react-icons/fa6";
-import {FcSmartphoneTablet} from "react-icons/fc"
+import { FaPaintbrush, FaDesktop, FaMobileScreenButton } from "react-icons/fa6";
+import { FcSmartphoneTablet } from "react-icons/fc";
+
 export const Work = {
-   
     ServicesIcons: [
         {
             id: 1,
             icons: FaPaintbrush,
             title: "Web Designing",
-            background:"servicebox1",
-            para:"Crafting digital experiences tailored to your brand's essence, fostering engagement and growth in the online realm."
-
+            background: "servicebox1",
+            para: "Leveraging advanced design skills to create visually appealing and user-friendly interfaces that captivate users and reflect the essence of your brand."
         },
         {
             id: 2,
             icons: FaDesktop,
             title: "Website Development",
-            background:"servicebox2",
-            para:"Building bespoke digital platforms that reflect your brand's identity, driving online success with user-focused design and functionality."
-
+            background: "servicebox2",
+            para: "Applying comprehensive development skills to build robust and scalable digital platforms, ensuring that your brand's vision is effectively translated into functional, high-performing websites."
         },
         {
             id: 3,
             icons: FcSmartphoneTablet,
             title: "Responsive Design",
-            background:"servicebox8",
-            para:"Crafting responsive designs for your website, guaranteeing accessibility and engagement across all device."
-
+            background: "servicebox8",
+            para: "Utilizing expertise in responsive design to create adaptable and seamless user experiences across all devices, ensuring your website is accessible and engaging for every visitor."
         },
-            
-      
         {
             id: 4,
             icons: FaMobileScreenButton,
-            title: "Website Maintinance",
-            background:"servicebox6",
-            para:"Providing ongoing support and updates to ensure websites remain secure, up-to-date, and functional.."
-
-
+            title: "Website Maintenance",
+            background: "servicebox6",
+            para: "Providing skillful maintenance and updates to keep websites secure, up-to-date, and performing optimally, helping to sustain long-term success and growth for your online presence."
         },
-      ],
-   
-
+    ],
 };
