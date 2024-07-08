ae042dc5a1367a06db3732fde4d620e32a0c3b3c
Asif Ali <aliiasif3958@gmail.com>
2024-07-08 10:10:59 +0530

    changes into footer bottom

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
         <div className=" text-center py-lg-4 py-3 mt-md-5 mt-3">
 
                 <div className="footer-info my-footer py-2 py-md-4 fw-medium">
-                <div className="copywrite"><h4>© 2024 All rights reserved ! Made  by Asif Ali</h4>
+                <div className="copywrite"><h6 className="mb-0">© 2024 All rights reserved ! Made  by Asif Ali</h6>
                 </div>
                 </div>
 
